c387c2730276417dc7c36349da79b99138365e92
Dawid Jeż <dawidjez@gmail.com>
2021-11-12 14:52:42 +0100

    svelte icon

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -15,19 +15,22 @@ class Project extends Model
     const TYPE_REACT = 'react';
     const TYPE_ANGULAR = 'angular';
     const TYPE_NODEJS = 'nodejs';
+    const TYPE_SVELTE = 'svelte';
 
     const TYPES = [
         self::TYPE_LARAVEL,
         self::TYPE_ANGULAR,
         self::TYPE_REACT,
-        self::TYPE_NODEJS
+        self::TYPE_NODEJS,
+        self::TYPE_SVELTE,
     ];
 
     const TYPES_NAMES = [
         self::TYPE_LARAVEL => 'Laravel',
         self::TYPE_ANGULAR => 'Angular',
         self::TYPE_REACT => 'React',
-        self::TYPE_NODEJS => 'NodeJS'
+        self::TYPE_NODEJS => 'NodeJS',
+        self::TYPE_SVELTE => 'Svelte',
     ];
 
     protected $fillable = [
